e9750d0b2603789b1fe8f9f03dc6802ffe3bdb02
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-01-24 13:03:27 +0200

    Initial Commit

---
+++ MultithreadingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultithreadingTest
+//
+//  Created by Oksana Poliakova on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
