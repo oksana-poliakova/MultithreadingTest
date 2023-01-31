d37bc2e2bbca3adf1b9755465d1185be9c7e0dbb
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-01-31 13:09:26 +0200

    Tested asyncAfter, concurrentPerform

---
+++ MultithreadingTest/GCD/DispatchWorkItemViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DispatchWorkItemViewController.swift
+//  MultithreadingTest
+//
+//  Created by Oksana Poliakova on 31.01.2023.
+//
+
+import UIKit
+
+class DispatchWorkItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- MultithreadingTest/GCD/GCDViewController.swift
+++ MultithreadingTest/GCD/GCDViewController.swift
@@ -17,13 +17,51 @@ final class GCDViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupUI()
+        useConcurrentPerform()
+        
+        getDataWithDelay(seconds: 5, queue: .main) {
+            print("Test code with delay")
+            self.showAlert()
+            print(Thread.current)
+        }
     }
     
     // MARK: - Setup UI
     
     private func setupUI() {
         title = "First ViewController"
+    }
+    
+    // MARK: - Async after
+    
+    private func getDataWithDelay(seconds: Int,
+                                  queue: DispatchQueue = DispatchQueue.global(),
+                                  completion: @escaping () -> ()) {
+        queue.asyncAfter(deadline: .now() + .seconds(seconds)) {
+            completion()
+        }
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Hello", message: "It's alert", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    // MARK: - Concurrent perform
+    /* Submits a single block to the dispatch queue and causes the block to be executed the specified number of times. */
+    
+    private func useConcurrentPerform() {
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            DispatchQueue.concurrentPerform(iterations: 200) {
+                print("\($0) times")
+                print(Thread.current)
+            }
+        }
     }
     
     // MARK: - IBActions
@@ -33,6 +71,5 @@ final class GCDViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
         print("pressed")
     }
-    
 }
 
